d6c0cce4eeed77f57a6794eba748c10740f4737b
Pratiksha <95347099+lastoyster@users.noreply.github.com>
2024-01-23 14:37:37 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# Flutter Bloc Tutorial
+
+###### Watch on YouTube 👇
+
+<a href='https://youtu.be/0wHTltfkQTI'><img src="./youtube.png" height="100" width="100"></a>
+
+## Following topics will be covered
+1. What is Bloc? (Business Logic Component)
+2. Cubit (Reduces the boilerplate code)
+3. Bloc Observer
+4. Bloc Provider & Multi Bloc Provider
+5. Bloc Builder 
+6. Bloc Listener & Multi Bloc Listener
+7. Bloc Selector
+8. Bloc Consumer
+9. Repository Provider
+10. Repository & Multiple Repository
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:bloc_tutorial/state/counter_state.dart';
+import 'package:bloc_tutorial/state_observer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  Bloc.observer = StateObserver();
+  runApp(MaterialApp(
+    home: BlocProvider(
+      create: (_) => CounterState(),
+      child: Home(),
+    ),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () => context.read<CounterState>().increament(),
+              child: Text('click'),
+            ),
+            /*  BlocBuilder<CounterState, int>(
+              builder: (context, state) => Text(state.toString()),
+              buildWhen: (previousState, newState) => newState > 10,
+            ),
+            //Series Part 2
+            BlocListener(
+              listener: (context, state) {
+                print('BlocListener $state');
+              },
+              child: BlocBuilder<CounterState, int>(
+              builder: (context, state) => Text(state.toString()),
+            ),
+              bloc: context.read<CounterState>(),
+              //bloc: BlocProvider.of<CounterState>(context),
+              listenWhen: (int previousState, int newState) => newState > 5,
+            ), */
+            
+            //Series Part 3
+            BlocConsumer<CounterState, int>(
+              builder: (context, state) => Text(state.toString()),
+              buildWhen: (previousState,newState)=> newState > 5,
+              listener: (context, state) {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text(state.toString())));
+              },
+              listenWhen: (previousState,newState)=> newState > 3,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
